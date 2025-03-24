b5e896c4551cec70245b5d6595012e012b2c953b
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-03-24 10:09:50 +0300

    adminlogin and weather functions implemented

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,30 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+admin_login("admin","12345")
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif (temperature >= 40 and temperature <= 65):
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+hows_the_weather(77)
 
 def fizzbuzz(num):
     # your code here
+    
     pass
 
 def calculator(operation, num1, num2):
     # your code here
     pass
+
